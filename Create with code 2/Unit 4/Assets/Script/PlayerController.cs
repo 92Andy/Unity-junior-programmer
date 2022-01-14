8afadd4f722c770d7b4214783357791614abae0f
Andy Eckhardt <eckhardtandy@gmail.com>
2022-01-14 22:43:45 +0100

    Added vertical player movement

---
+++ Create with code 2/Unit 4/Assets/Script/PlayerController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    private float speed = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var verticalInput = Input.GetAxis("Vertical");
+        playerRb.AddForce(Vector3.forward * verticalInput * speed);
+    }
+}
