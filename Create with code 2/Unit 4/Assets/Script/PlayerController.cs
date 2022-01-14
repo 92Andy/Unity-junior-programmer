a8f2a1eb79bb4b5a5c439b358d2eaba5fe5e956a
Andy Eckhardt <eckhardtandy@gmail.com>
2022-01-14 22:48:17 +0100

    move in direction of focal point

--- Create with code 2/Unit 4/Assets/Script/PlayerController.cs
+++ Create with code 2/Unit 4/Assets/Script/PlayerController.cs
@@ -5,17 +5,20 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
+    private GameObject focalPoint;
     private float speed = 5f;
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("Focal Point");
     }
 
     // Update is called once per frame
     void Update()
     {
         var verticalInput = Input.GetAxis("Vertical");
-        playerRb.AddForce(Vector3.forward * verticalInput * speed);
+        Vector3 direction = focalPoint.transform.forward;
+        playerRb.AddForce(direction * verticalInput * speed);
     }
 }
