008518332a3e23ac43b193eeaba9037eb82bf5f4
Andy Eckhardt <eckhardtandy@gmail.com>
2022-01-14 22:39:24 +0100

    camera rotation speed improved

--- Create with code 2/Unit 4/Assets/Script/RotateCamera.cs
+++ Create with code 2/Unit 4/Assets/Script/RotateCamera.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class RotateCamera : MonoBehaviour
 {
-    public float rotationSpeed = 10f;
+    private float rotationSpeed = 25f;
 
     // Start is called before the first frame update
     void Start()
