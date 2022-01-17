fc9cb4eef5eb62be6ee90d800b94fd0c3581de9a
Andy Eckhardt <eckhardtandy@gmail.com>
2022-01-17 19:07:20 +0100

    Added PowerUp logic
    If the player picks the power up, he can push the enemy away in the opposite direction.

--- Create with code 2/Unit 4/Assets/Script/PlayerController.cs
+++ Create with code 2/Unit 4/Assets/Script/PlayerController.cs
@@ -7,6 +7,9 @@ public class PlayerController : MonoBehaviour
     private Rigidbody playerRb;
     private GameObject focalPoint;
     private float speed = 5f;
+
+    public bool pickedPowerUp = false;
+    private float powerUpStrength = 10f;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,5 +23,37 @@ public class PlayerController : MonoBehaviour
         var verticalInput = Input.GetAxis("Vertical");
         Vector3 direction = focalPoint.transform.forward;
         playerRb.AddForce(direction * verticalInput * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("PowerUp"))
+        {
+            StartCoroutine(HandlePowerUpPickedRoutine());
+            Destroy(other.gameObject);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy") && pickedPowerUp)
+        {
+            PushEnemyAway(collision.gameObject);
+        }
+    }
+
+    private IEnumerator HandlePowerUpPickedRoutine()
+    {
+        pickedPowerUp = true;
+        yield return new WaitForSeconds(7);
+        pickedPowerUp = false;
+    }
+
+    private void PushEnemyAway(GameObject enemy)
+    {
+        var enemyRb = enemy.GetComponent<Rigidbody>();
+        var awayFromPlayer = enemyRb.position - transform.position;
+
+        enemyRb.AddForce(awayFromPlayer * powerUpStrength, ForceMode.Impulse);
     }
 }
