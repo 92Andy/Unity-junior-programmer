c40188dc929e5ac946077e675cf45dd8a68c15fb
Andy Eckhardt <eckhardtandy@gmail.com>
2022-01-17 18:50:39 +0100

    Added rotated power up item

---
+++ Create with code 2/Unit 4/Assets/Script/RotationMovement.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationMovement : MonoBehaviour
+{
+    public float rotationSpeed = 20f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
+    }
+}
