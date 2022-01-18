3f33ce57a41c9dd5bed1b2a6719cf3b57075e88b
Andy Eckhardt <eckhardtandy@gmail.com>
2022-01-18 19:47:00 +0100

    Added enemy destruction

--- Create with code 2/Unit 4/Assets/Script/Enemy.cs
+++ Create with code 2/Unit 4/Assets/Script/Enemy.cs
@@ -17,7 +17,21 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        MoveToPlayer();
+        DestoryOnOutOfBound();
+    }
+
+    private void MoveToPlayer()
+    {
         var directionToPlayer = (player.transform.position - transform.position).normalized;
         enemyRb.AddForce(directionToPlayer * speed);
+    }
+
+    private void DestoryOnOutOfBound()
+    {
+        if (transform.position.y < -10)
+        {
+            Destroy(this.gameObject);
+        }
     }
 }
