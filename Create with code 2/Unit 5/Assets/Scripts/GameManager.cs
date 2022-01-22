cb53dcd85e7f03c9968e95197763feb558f01067
Andy Eckhardt <eckhardtandy@gmail.com>
2022-01-22 18:28:48 +0100

    Added Target behaviour and GameManager

--- Create with code 2/Unit 5/Assets/Scripts/GameManager.cs
+++ Create with code 2/Unit 5/Assets/Scripts/GameManager.cs
@@ -1,13 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public List<GameObject> targets = new List<GameObject>();
+
+    private float spawnRate = 1f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (targets.Count == 0)
+            throw new MissingComponentException("Their are no Targets!");
+        StartCoroutine(SpawnTargets());
+    }
+
+    private IEnumerator SpawnTargets()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            var randomIndex = Random.Range(0, targets.Count);
+            Instantiate(targets.ElementAt(randomIndex));
+        }
     }
 
     // Update is called once per frame
--- Create with code 2/Unit 5/Assets/Scripts/Target.cs
+++ Create with code 2/Unit 5/Assets/Scripts/Target.cs
@@ -1,18 +1,56 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Target : MonoBehaviour
 {
+    private Rigidbody targetRb;
+
+    private float spawnYPos = -2;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        targetRb = GetComponent<Rigidbody>();
+        SpawnAndMoveTarget();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void SpawnAndMoveTarget()
     {
-        
+        transform.position = RandomSpawnPos();
+        targetRb.AddForce(RandomUpForce(), ForceMode.Impulse);
+        targetRb.AddTorque(RandomTorqueForce());
+    }
+
+    private Vector3 RandomTorqueForce()
+    {
+        var randomX = UnityEngine.Random.Range(-10f, 10f);
+        var randomY = UnityEngine.Random.Range(-10f, 10f);
+        var randomZ = UnityEngine.Random.Range(-10f, 10f);
+        return new Vector3(randomX, randomY, randomZ);
+    }
+
+    private Vector3 RandomSpawnPos()
+    {
+        return new Vector3(UnityEngine.Random.Range(-10f, 10f), spawnYPos);
+    }
+
+    private Vector3 RandomUpForce()
+    {
+        return Vector3.up * UnityEngine.Random.Range(10f, 16f);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Sensor"))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        Destroy(gameObject);
     }
 }
