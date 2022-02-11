952e08675879602faa30b46b189c93cd1b958695
Andy Eckhardt <eckhardtandy@gmail.com>
2022-02-11 20:00:14 +0100

    added difficultyButton scaffold

---
+++ Create with code 2/Unit 5/Assets/Scripts/DifficultyButton.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DifficultyButton : MonoBehaviour
+{
+    private Button difficultyButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        difficultyButton = GetComponent<Button>();
+        difficultyButton.onClick.AddListener(SetDifficulty);
+    }
+
+    private void SetDifficulty()
+    {
+        Debug.Log(gameObject.name + " was clicked");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
